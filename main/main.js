92ae0985e8aa939d7700f035e8c6fc9cd7f1f6a5
cc <chezenan@qq.com>
2019-05-29 22:23:04 +0800

    modify main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,35 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(str) {
+    var len = str.length;
+	var numHead = ["._.", "...", "._.", "._.","...","._.","._.","._.","._.","._."];
+	var numMid = ["|.|","..|","._|","._|","|_|","|_.","|_.","..|","|_|","|_|"];
+	var numFoot = ["|_|","..|","|_.","._|","..|","._|","|_|","..|","|_|","..|"];
+	var result = "";
+	for(var i=0;i<len;i++){
+		var index = parseInt(str.charAt(i));
+		result = result + numHead[index];
+		if(i<len-1){
+			result = result + " ";
+		}else{
+			result = result + "\n";
+		}
+	}
+	for(var i=0;i<len;i++){
+		var index = parseInt(str.charAt(i));
+		result = result + numMid[index];
+		if(i<len-1){
+			result = result + " ";
+		}else{
+			result = result + "\n";
+		}
+	}
+	for(var i=0;i<len;i++){
+		var index = parseInt(str.charAt(i));
+		result = result + numFoot[index];
+		if(i<len-1){
+			result = result + " ";
+		}else{
+			result = result + "\n";
+		}
+	}
+    return result;
 };
